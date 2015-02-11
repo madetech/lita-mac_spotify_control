4634bb2b78083d6977818c923715e07624471921
Seb Ashton <seb@madebymade.co.uk>
2015-02-11 10:10:18 +0000

    Skeleton of Spotify::Search

---
+++ lib/spotify/search.rb
@@ -0,0 +1,24 @@
+require 'rspotify'
+
+module Spotify
+  class Search
+    def initialize
+    end
+
+    def artist(artist_name)
+
+    end
+
+    def album(album_name)
+
+    end
+
+    def playlist(playlist_name)
+
+    end
+
+    def track(track_name)
+
+    end
+  end
+end
