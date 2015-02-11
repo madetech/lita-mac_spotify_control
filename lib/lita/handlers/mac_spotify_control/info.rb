b390a99890f84dce6d960c4498c38f44f5bd5dd5
Seb Ashton <seb@madebymade.co.uk>
2015-02-11 09:01:30 +0000

    Add info commands

---
+++ lib/lita/handlers/mac_spotify_control/info.rb
@@ -0,0 +1,25 @@
+module Lita::Handlers::MacSpotifyControl
+  class Info < Lita::Handler
+    namespace "mac_spotify_control"
+
+    route(%r{^(?:info|whats playing|what is playing|shazzam)$},
+           :info,
+           command: true,
+           help: { 'info|whats playing|what is playing|shazzam' => 'Info about the current track'})
+
+    route(%r{^(?:search|look for|find me) (.+)$},
+           :search,
+           command: true,
+           help: { 'search|look for|find me QUERY' => 'Search for QUERY'})
+
+    def search(response)
+      # noop
+    end
+
+    def info(response)
+      # noop
+    end
+  end
+end
+
+Lita.register_handler(Lita::Handlers::MacSpotifyControl::Info)
---
+++ spec/lita/handlers/mac_spotify_control/info_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Lita::Handlers::MacSpotifyControl::Info, lita_handler: true do
+  context 'info/whats playing/shazzam' do
+    it { is_expected.to route_command('info').to(:info) }
+    it { is_expected.to route_command('whats playing').to(:info) }
+    it { is_expected.to route_command('shazzam').to(:info) }
+  end
+
+  context 'search/look for/find me' do
+    it { is_expected.to route_command('search New Found Glory').to(:search) }
+    it { is_expected.to route_command('look for The Starting Line').to(:search) }
+    it { is_expected.to route_command('find me Rufio').to(:search) }
+  end
+end
