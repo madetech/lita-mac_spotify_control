b8dc5ee673848d2a979efa9c97b6e8e2b4aeaa4c
Seb Ashton <seb@madebymade.co.uk>
2015-02-11 09:50:03 +0000

    Use open3 to return stdout

--- lib/ruby_osascript.rb
+++ lib/ruby_osascript.rb
@@ -1,3 +1,13 @@
+require 'open3'
+require 'ostruct'
+
 def osascript(script)
-  system('osascript', *script.split(/\n/).map { |line| ['-e', line] }.flatten)
+  stdout, stderr, status = Open3.capture3('osascript',
+                                          *script.split(/\n/).map { |line| ['-e', line] }.flatten)
+
+  OpenStruct.new(
+    stdout: stdout,
+    stderr: stderr,
+    status: status
+  )
 end
